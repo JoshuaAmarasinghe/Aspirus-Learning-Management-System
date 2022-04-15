fe9b4e458a4d95624e8e33faa2ac00c2fbfc8fa7
IT20073978 <89725167+IT20073978@users.noreply.github.com>
2022-04-15 10:41:11 +0530

    add notse and event direction path

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import AllStaffs from './components/StaffManagement/AllStaffs/AllStaffs';
 import ViewNoticesAndEventsDetails from './components/noticeandevent-manager/ViewAllNoticesAndEvents';
 import ViewOneNoticeOrEvent from './components/noticeandevent-manager/ViewOneNoticeOrEvent';
 import AddNoticeOrEvent from './components/noticeandevent-manager/AddNoticeOrEvent';
+import NoticeOrEventNAV from './Routes/NoticeAndEventRoutes';
 
 
 function App() {
@@ -25,9 +26,13 @@ function App() {
               <Route exact path="/staff/profile" element={<Profile/>} />
               <Route exact path="/staff/update/:id" element={<UpdateProfile/>} />
               <Route exact path="/staff" element={<AllStaffs/>} />
-            <Route exact path = "/noticeandeventManager/add" element={<AddNoticeOrEvent/>}/>
-            <Route exact path = "/noticeandeventManager/view" element={<ViewNoticesAndEventsDetails/>}/>
-            <Route exact path = "/noticeandeventManager/view/:id" element={<ViewOneNoticeOrEvent/>}/>
+              <Route exact path = "/noticeandeventManager/add" element={<AddNoticeOrEvent/>}/>
+              <Route exact path = "/noticeandeventManager/view" element={<ViewNoticesAndEventsDetails/>}/>
+              <Route exact path = "/noticeandeventManager/view/:id" element={<ViewOneNoticeOrEvent/>}/>
+              <Route exact path = "/noticeandeventManager" element={<NoticeOrEventNAV/>}/>
+              <Route exact path = "/noticeandeventManager/add" element={<AddNoticeOrEvent/>}/>
+              <Route exact path = "/noticeandeventManager/view" element={<ViewNoticesAndEventsDetails/>}/>
+              <Route exact path = "/noticeandeventManager/view/:id" element={<ViewOneNoticeOrEvent/>}/>
         </Routes>
        </div>
     </Router>
