bd5e914ef54527e21767ede1f3d2538b6584cd1f
joshuaamaamarasinghe <joshuaauaamarasinghe@gmail.com>
2022-04-16 11:49:21 +0530

    implemented homepage

--- client/src/components/Home/Homepage.js
+++ client/src/components/Home/Homepage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Slider from "./Slider/Slider";
+import About from "./About/About";
+import Services from "./Services/Services";
 
 function Homepage() {
 
     return (
         <div className="container">
+            <Slider/>
+            <About/>
+            <Services/>
         </div>
     )
 }
